85416260ba2ff86b8729e9bd1d2ee187e36d284a
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-02-11 16:09:14 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
         Department dep = new Department(1, "Books");
-
-
         System.out.println(dep);
 
+        Seller seller = new Seller(10, "Bob", "bob@gmail.com", LocalDate.now(), 4500.0, dep);
+        System.out.println(seller);
     }
 }
